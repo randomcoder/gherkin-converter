e960ebbc14f486b54f63973f34728dbd79c3e804
Tim Sheppard <tim.sheppard@anatwine.com>
2017-04-20 08:59:52 +0100

    Update default html and styling

--- src/main/scala/uk/co/randomcoding/cucumber/generator/writer/FeatureHtml.scala
+++ src/main/scala/uk/co/randomcoding/cucumber/generator/writer/FeatureHtml.scala
@@ -1,6 +1,6 @@
 package uk.co.randomcoding.cucumber.generator.writer
 
-import uk.co.randomcoding.cucumber.generator.gherkin.{Examples, Feature, ScenarioDesc}
+import uk.co.randomcoding.cucumber.generator.gherkin.{Examples, Feature, ScenarioDesc, ScenarioOutline}
 
 import scala.io.Source
 import scala.xml.NodeSeq
@@ -10,9 +10,11 @@ object FeatureHtml {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <title>Feature: {feature.description}</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"/>
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
         <style type="text/css">
@@ -20,47 +22,35 @@ object FeatureHtml {
         </style>
       </head>
       <body>
-        <div class="container feature">
-          <div class="row feature-title">
-            <div class="col col-sm-8 col-sm-offset-2">{ if(feature.tags.nonEmpty) {
-              <div class="row" id="feature_tags">
-                <div class="col-sm-12 tags">
+        <div class="container">
+          <div class="row">
+            <div class="col col-sm-10 col-sm-offset-1 feature">{ if (feature.tags.nonEmpty) {
+              <div class="row feature_tags" id="feature_tags">
+                <div class="col-sm-12">
                   {feature.tags.mkString(" ")}
                 </div>
               </div>}}
               <div class="row">
-                <div class="col-sm-3">
-                  <h2>Feature:</h2>
-                </div>
-                <div class="col col-sm-6 feature_description" id="feature_description">
-                  {feature.description}
+                <div class="col-sm-12" id="feature_description">
+                  <span class="feature_heading">Feature: </span><span class="feature_description">{feature.description}</span>
                 </div>
               </div>
               <div class="row">
-                <div class="col-sm-3">
-                  <h4>As a</h4>
-                </div>
-                <div class="col col-sm-6 feature_part" id="as_a">
-                  {feature.asA}
+                <div class="col-sm-12" id="as_a">
+                  <span class="feature_part_prefix">As a </span><span class="feature_part">{feature.asA}</span>
                 </div>
               </div>
               <div class="row">
-                <div class="col-sm-3">
-                  <h4>I Want</h4>
-                </div>
-                <div class="col col-sm-6 feature_part" id="i_want">
-                  {feature.iWantTo}
+                <div class="col-sm-12" id="i_want">
+                  <span class="feature_part_prefix">I want </span><span class="feature_part" >{feature.iWantTo}</span>
                 </div>
               </div>
               <div class="row">
-                <div class="col-sm-3">
-                  <h4>In order to</h4>
-                </div>
-                <div class="col col-sm-6 feature_part" id="in_order_to">
-                  {feature.inOrderTo}
+                <div class="col-sm-2 feature_part_prefix" id="in_order_to">
+                  <span class="feature_part_prefix">In order to </span><span class="feature_part">{feature.inOrderTo}</span>
                 </div>
               </div>
-              { feature.scenarios.zipWithIndex.map{ case (scenario, index) => scenarioHtml(scenario, s"scenario_$index")} }
+              { feature.scenarios.zipWithIndex.map { case (scenario, index) => scenarioHtml(scenario, s"scenario_$index") } }
             </div>
           </div>
         </div>
@@ -69,23 +59,33 @@ object FeatureHtml {
   }
 
   private[this] def scenarioHtml(scenario: ScenarioDesc, elementId: String) = {
-    <div class="row">
-      <div class="col-sm-3">
-        <h3>{scenario.identifier}:</h3>
+    <div class="scenario">{ if (scenario.tags.nonEmpty) {
+      <div class="row" id="scenario_tags">
+        <div class="col-sm-12 scenario_tags">
+          {scenario.tags.mkString(" ")}
+        </div>
+      </div>}}
+      <div class="row">
+        <div class="col-sm-12" id="scenario_description">
+          <span class="scenario_heading">{scenario.identifier}: </span>
+          <span class="scenario_description">{scenario.description}</span>
+        </div>
       </div>
-      <div class="col col-sm-6 scenario_description" id="scenario_description">
-        {scenario.description}
-      </div>
-    </div> ++
-    {stepsHtml(scenario.givens, "given")} ++
-    {stepsHtml(scenario.whens, "when")} ++
-    {stepsHtml(scenario.thens, "then")}
+      {stepsHtml(scenario.givens, "given") ++
+      stepsHtml(scenario.whens, "when") ++
+      stepsHtml(scenario.thens, "then") ++
+      (scenario match {
+        case ScenarioOutline(_, _, _, _, _, examples) => examplesHtml(examples)
+        case _ => Nil
+      })
+      }
+    </div>
   }
 
-  private[this] def stepsHtml(steps: Seq[String], id: String) = {
-    steps.flatMap{ step =>
+  private[this] def stepsHtml(steps: Seq[String], gwtClass: String) = {
+    steps.flatMap { step =>
       <div class="row">
-        <div class="col col-sm-8 given">
+        <div class={s"col col-sm-12 $gwtClass"}>
           {step}
         </div>
       </div>
@@ -93,9 +93,9 @@ object FeatureHtml {
   }
 
   private[this] def examplesHtml(examples: Examples) = {
-    <div class="examples">{ if(examples.tags.nonEmpty) {
+    <div class="examples">{ if (examples.tags.nonEmpty) {
       <div class="row" id="example_tags">
-        <div class="col-sm-12 tags">
+        <div class="col-sm-12 example_tags">
           {examples.tags.mkString(" ")}
         </div>
       </div>}}
@@ -107,8 +107,8 @@ object FeatureHtml {
       <div class="row">
         <div class="col col-sm-8">
           <table>
-            <tr>{ examples.headings.map(heading => <th>{heading}</th>) }</tr> ++
-            { examples.examples.flatMap{ example => <tr>{example.map(ex => <td>{ex}</td>) }</tr> } }
+            <tr>{ examples.headings.map(heading => <th>{heading}</th>) }</tr>
+            { examples.examples.flatMap { example => <tr>{example.map(ex => <td>{ex}</td>) }</tr> } }
           </table>
         </div>
       </div>
--- src/test/scala/uk/co/randomcoding/cucumber/generator/writer/FeatureHtmlSpec.scala
+++ src/test/scala/uk/co/randomcoding/cucumber/generator/writer/FeatureHtmlSpec.scala
@@ -16,34 +16,34 @@ class FeatureHtmlSpec extends FlatSpecTest with StreamlinedXmlNormMethods {
     (page \ "head" \ "title").text should be(s"Feature: ${feature.description}")
   }
 
-  it should "put the feature description into a div with the id 'feature_description" in {
+  it should "put the feature description into a div with the id 'feature_description'" in {
     val feature = Feature("Sample feature for test", "", "", "", Nil, Nil)
-    FeatureHtml(feature).norm.divWithId("feature_description").text should be(feature.description)
+    FeatureHtml(feature).divWithId("feature_description").text.trim should be(s"Feature: ${feature.description}")
   }
 
-  it should "put the As A text into a div with the id 'as_a" in {
-    val feature = Feature("", "big old tester", "", "", Nil, Nil)
-    FeatureHtml(feature).norm.divWithId("as_a").text should be(feature.asA)
+  it should "put the As A text into a div with the id 'as_a'" in {
+    val feature = Feature("", "", "big old tester", "", Nil, Nil)
+    FeatureHtml(feature).divWithId("as_a").text.trim should be(s"As a ${feature.asA}")
   }
 
-  it should "put the I Want text into a div with the id 'i_want" in {
-    val feature = Feature("", "", "this to be right", "", Nil, Nil)
-    FeatureHtml(feature).norm.divWithId("i_want").text should be(feature.iWantTo)
+  it should "put the I Want text into a div with the id 'i_want'" in {
+    val feature = Feature("", "", "", "this to be right", Nil, Nil)
+    FeatureHtml(feature).divWithId("i_want").text.trim should be(s"I want ${feature.iWantTo}")
   }
 
-  it should "put the In Order To text into a div with the id 'in_order_to" in {
-    val feature = Feature("", "", "", "make nice gherkins", Nil, Nil)
-    FeatureHtml(feature).norm.divWithId("in_order_to").text should be(feature.inOrderTo)
+  it should "put the In Order To text into a div with the id 'in_order_to'" in {
+    val feature = Feature("", "make nice gherkins", "", "", Nil, Nil)
+    FeatureHtml(feature).divWithId("in_order_to").text.trim should be(s"In order to ${feature.inOrderTo}")
   }
 
   it should "not have a row for the feature tags if the feature has no tags" in {
     val feature = Feature("", "", "", "", Nil, Nil)
-    FeatureHtml(feature).norm.divWithId("feature_tags") should be('empty)
+    FeatureHtml(feature).divWithId("feature_tags") should be('empty)
   }
 
   it should "display the feature tags in a div with the id 'feature_tags' if the feature has tags" in {
     val feature = Feature("", "", "", "", Seq("@tag1", "@tag2"), Nil)
-    FeatureHtml(feature).norm.divWithId("feature_tags").text should be("@tag1 @tag2")
+    FeatureHtml(feature).divWithId("feature_tags").text.trim should be("@tag1 @tag2")
   }
 
   implicit class RichHtml(val html: NodeSeq) extends AnyRef {
